857271333005ad617bb785ba5c6d575c8615ac6c
Vo Hieu Thang <hieuthang369@gmial.com>
2021-12-06 23:09:18 +0700

    section 5 100%

--- src/script/home.js
+++ src/script/home.js
@@ -9,13 +9,20 @@ $(function () {
         $(this).addClass("active")
         const imgActive = $(".gallery-item.active > img");
         newURL = `url('${imgActive.attr('src')}')`;
-        $('.exhibitions-container').css('background-image', newURL)
+        $('.exhibitions-container').css({
+            'background-image': newURL
+        })
     })
     // SLIDE HANDLER
     const galleryItemAmount = galleryItem.length;
+    const firstItem = galleryItem[0];
+    const lastItem = galleryItem[galleryItem.length - 1];
     let galleryCount;
+    //Media loaded handle
     $(window).on('load', function () {
         galleryCount = Math.round(galleryContainer.width() / galleryItem.width())
+        const { right } = lastItem.getBoundingClientRect();
+        const { left } = firstItem.getBoundingClientRect();
     })
     let translateValue = 0;
     nextBtn.click(function () {
@@ -23,14 +30,33 @@ $(function () {
         galleryContainer.css({
             transform: `translateX(${translateValue}%)`
         })
+        const { right } = lastItem.getBoundingClientRect();
+        const { left } = firstItem.getBoundingClientRect();
+        console.log("left", left);
+        console.log("right", right);
     })
     prevBtn.click(function () {
         translateValue += 100 / galleryCount;
         galleryContainer.css({
             transform: `translateX(${translateValue}%)`
         })
+        const { right } = lastItem.getBoundingClientRect();
+        const { left } = firstItem.getBoundingClientRect();
+        console.log("left", left);
+        console.log("right", right);
     })
 
 })
-
-// SECTION 4 -  END  ========================
+// SECTION 5 -  START  ========================
+$(function () {
+    $('.visit-img').click(function () {
+        $(this).children('.visit-overlay').css('visibility', 'visible');
+    })
+    $('.visit-overlay').click(function (e) {
+        e.stopPropagation();
+        $(this).css('visibility', 'hidden');
+        $(this).children('img').click(function (e) {
+            e.stopPropagation();
+        })
+    })
+})

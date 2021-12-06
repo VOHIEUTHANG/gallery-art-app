d29cfed5ac148e52e31978c44267ab1bbfc536f5
Vo Hieu Thang <hieuthang369@gmial.com>
2021-12-06 13:19:11 +0700

    slide show 50%

--- src/script/home.js
+++ src/script/home.js
@@ -1,13 +1,44 @@
 // SECTION 4 - START ========================
 $(function () {
     const galleryItem = $(".gallery-item");
+    const galleryContainer = $(".exhibitions-gallery");
+    const nextBtn = $(".gallery-ct-next");
+    const prevBtn = $(".gallery-ct-prev");
     galleryItem.click(function () {
         $(".gallery-item.active").removeClass("active");
         $(this).addClass("active")
         const imgActive = $(".gallery-item.active > img");
         newURL = `url('${imgActive.attr('src')}')`;
-        console.log(newURL);
         $('.exhibitions-container').css('background-image', newURL)
     })
+    // SLIDE HANDLER
+    console.log('123');
+    console.log(galleryItem.width());
+    console.log(galleryContainer.width());
+    const galleryItemAmount = galleryItem.length;
+    const galleryCount = Math.round(galleryContainer.width() / galleryItem.width())
+    let translateValue = 0;
+    nextBtn.click(function () {
+        translateValue -= 100 / galleryCount;
+        console.log(translateValue);
+        galleryContainer.css({
+            transform: `translateX(${translateValue}%)`
+        })
+    })
+    prevBtn.click(function () {
+        translateValue += 100 / galleryCount;
+        console.log(translateValue);
+        galleryContainer.css({
+            transform: `translateX(${translateValue}%)`
+        })
+        galleryContainer.css({
+
+        })
+    })
+
 })
+
+console.log($(window));
+
+
 // SECTION 4 -  END  ========================
